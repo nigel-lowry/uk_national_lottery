9e0e8d33521b650f04b9e9e93c9a8dfd10c3dc8a
Nigel Lowry <nigel@lemmata.co.uk>
2019-04-06 12:27:25 +0100

    Some cleanup

--- spec/uk_national_lottery_spec.cr
+++ spec/uk_national_lottery_spec.cr
@@ -2,8 +2,8 @@ require "./spec_helper"
 
 describe UkNationalLottery do
   it "picks 6 balls" do
-    ld = UkNationalLottery::UkNationalLottery.new
-    ld.lucky_dip.size.should eq 6
+    lottery = UkNationalLottery::UkNationalLottery.new
+    lottery.lucky_dip.size.should eq 6
   end
 
   it "picks 6 unique balls" do
@@ -14,10 +14,10 @@ describe UkNationalLottery do
   end
 
   it "picks balls between 1 and 59 (inclusive)" do
-    ld = UkNationalLottery::UkNationalLottery.new
-    dip = ld.lucky_dip 
+    lottery = UkNationalLottery::UkNationalLottery.new
+    lucky_dip = lottery.lucky_dip 
     range = 1..59
-    dip.all? { |ball| range.includes? ball }.should eq true
+    lucky_dip.all? { |ball| range.includes? ball }.should eq true
   end
 
   it "gives a different set of balls next time" do
