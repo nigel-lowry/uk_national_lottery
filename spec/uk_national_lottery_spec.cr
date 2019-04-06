606bf5ff6278edb8344d45bc48cca194abe1b192
Nigel Lowry <nigel@lemmata.co.uk>
2019-04-06 12:36:48 +0100

    Put in block

--- spec/uk_national_lottery_spec.cr
+++ spec/uk_national_lottery_spec.cr
@@ -1,38 +1,39 @@
 require "./spec_helper"
 
 describe UkNationalLottery do
-  it "picks 6 balls" do
-    lottery = UkNationalLottery::UkNationalLottery.new
-    lottery.lucky_dip.size.should eq 6
+  describe "#lucky_dip" do
+    it "picks 6 balls" do
+      lottery = UkNationalLottery::UkNationalLottery.new
+      lottery.lucky_dip.size.should eq 6
+    end
+
+    it "picks 6 unique balls" do
+      lottery = UkNationalLottery::UkNationalLottery.new
+      lucky_dip = lottery.lucky_dip
+
+      lucky_dip.should eq lucky_dip.uniq
+    end
+
+    it "picks balls between 1 and 59 (inclusive)" do
+      lottery = UkNationalLottery::UkNationalLottery.new
+      lucky_dip = lottery.lucky_dip 
+      range = 1..59
+      lucky_dip.all? { |ball| range.includes? ball }.should eq true
+    end
+
+    it "gives a different set of balls next time" do
+      lottery = UkNationalLottery::UkNationalLottery.new
+      lucky_dip1 = lottery.lucky_dip
+      lucky_dip2 = lottery.lucky_dip
+
+      lucky_dip1.should_not eq lucky_dip2
+    end
+
+    it "sorts the balls in ascending order" do
+      lottery = UkNationalLottery::UkNationalLottery.new
+      lucky_dip = lottery.lucky_dip
+
+      lucky_dip.should eq lucky_dip.sort
+    end
   end
-
-  it "picks 6 unique balls" do
-    lottery = UkNationalLottery::UkNationalLottery.new
-    lucky_dip = lottery.lucky_dip
-
-    lucky_dip.should eq lucky_dip.uniq
-  end
-
-  it "picks balls between 1 and 59 (inclusive)" do
-    lottery = UkNationalLottery::UkNationalLottery.new
-    lucky_dip = lottery.lucky_dip 
-    range = 1..59
-    lucky_dip.all? { |ball| range.includes? ball }.should eq true
-  end
-
-  it "gives a different set of balls next time" do
-    lottery = UkNationalLottery::UkNationalLottery.new
-    lucky_dip1 = lottery.lucky_dip
-    lucky_dip2 = lottery.lucky_dip
-
-    lucky_dip1.should_not eq lucky_dip2
-  end
-
-  it "sorts the balls in ascending order" do
-    lottery = UkNationalLottery::UkNationalLottery.new
-    lucky_dip = lottery.lucky_dip
-
-    lucky_dip.should eq lucky_dip.sort
-  end
-
 end
